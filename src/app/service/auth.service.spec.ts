2fba3148093cd3bb9877f8a73a1ec6e496e14eb1
Himala Wijesiri <himchakum@gmail.com>
2023-01-31 00:39:50 +0530

    CI

--- src/app/service/auth.service.spec.ts
+++ src/app/service/auth.service.spec.ts
@@ -2,7 +2,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
@@ -44,7 +44,7 @@ fdescribe('AuthService', () => {
     req.flush({ success: true });
   });
 
-  it('should insert user data into the database when logged in', () => {
+  it('should send user info into the database and check when logged in', () => {
     const data = {
       email: 'asifkhan@gmail.com',
       password: 'asif1234',
@@ -149,4 +149,139 @@ fdescribe('AuthService', () => {
     const req = httpMock.expectOne('http://localhost:8080/auth/');
     expect(req.request.method).toBe('GET');
   });
+
+  it('should get the latest 4 vehicle list', () => {
+    service.getlast4().subscribe((result) => {
+      expect(result).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(
+      'http://localhost:8080/auth/latest-vehicles'
+    );
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should be created', inject([AuthService], (service: AuthService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should retrieve vehicle from database using an ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      service.singlevehicle(id).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/edit-vehicle/' + id
+      );
+      expect(req.request.method).toBe('GET');
+    }
+  ));
+
+  it('should retrieve vehicle details from database using an ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      service.vehicledetails(id).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/details/view-details/' + id
+      );
+      expect(req.request.method).toBe('GET');
+    }
+  ));
+
+  it('should retrieve vehicles detail records individualy from database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      service.vehicledetailrecord(id).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/edit-vehicle-detail-records/' + id
+      );
+      expect(req.request.method).toBe('GET');
+    }
+  ));
+
+  it('should delete vehicles from database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      service.deletevehicle(id).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/del-vehicle/' + id
+      );
+      expect(req.request.method).toBe('DELETE');
+    }
+  ));
+
+  it('should delete vehicles details from database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      service.deldetail(id).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/del-detail/' + id
+      );
+      expect(req.request.method).toBe('DELETE');
+    }
+  ));
+
+  it('should update vehicles from database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      const vehicle = 'Audi i8';
+      service.updatevehicle(id, vehicle).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/update-vehicle/' + id,
+        vehicle
+      );
+      expect(req.request.method).toBe('PUT');
+    }
+  ));
+
+  it('should update vehicles details from database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      const detail = '63b6a5b812049541eb2461ba';
+
+      service.updatedetail(id, detail).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/update-vehicle-records/' + id,
+        detail
+      );
+      expect(req.request.method).toBe('PUT');
+    }
+  ));
+
+  it('should update vehicles in database by ID', inject(
+    [AuthService],
+    (service: AuthService) => {
+      const vehicle = 'Audi i8';
+
+      service.updatevehicle(id, vehicle).subscribe((data) => {
+        expect(data).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(
+        'http://localhost:8080/auth/update-vehicle/' + id,
+        vehicle
+      );
+      expect(req.request.method).toBe('PUT');
+    }
+  ));
 });
